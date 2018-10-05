305f42e6433445254da5afd6f3b3251ee43d7f3f
Steven Humphrey <steven.humphrey@bbc.co.uk>
2018-10-05 16:27:45 +0100

    A little hpme controller

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import path from 'path';
 
+import { index as home } from './controllers/home';
+
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -11,6 +13,8 @@ app.set('view engine', 'pug');
 app.use(
     express.static(path.join(__dirname, '../public'), { maxAge: 31557600000 }),
 );
+
+app.get('/', home);
 
 export default app;
 
